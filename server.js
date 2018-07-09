b32fbf707dc4da164b11e1081ef49af04ffd0a93
Karolis <karolis.strazdas@ktu.edu>
2018-07-09 13:41:59 +0300

    Update server.js

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ app.get('/api', function (req, res) {
 // Serve static assets if in production.
 if(process.env.NODE_ENV === 'production'){
     //Set static folder.
-    app.use(express.static('car-rent-client/build'));
+    app.use(express.static('client/build'));
     
     app.get('*', (req, res) => {
         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
